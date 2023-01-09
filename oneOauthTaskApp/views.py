d3f71ffba38c0cf1de56f31d95cf9ebf5c1cfe43
JoeAtMicrosoft <v-jsessions@microsoft.com>
2023-01-09 13:48:51 -0800

    Token attempt 2

--- oneOauthTaskApp/views.py
+++ oneOauthTaskApp/views.py
@@ -1,41 +1,42 @@
 from django.shortcuts import render
 import requests
 
-# Create your views here.
 def home(request):
     url = """https://app.vssps.visualstudio.com/oauth2/authorize?client_id=4849160F-C8F2-43C2-AB32-3EE2F7D6DD54&response_type=Assertion&state=User1&scope=vso.taskgroups_write&redirect_uri=https://ado-oauth.azurewebsites.net/oauth""" 
     print(url)
     return render(request, 'oneOauthTaskApp/home.html', {"url":url})
 
+    # https://learn.microsoft.com/en-us/azure/devops/integrate/get-started/authentication/oauth?view=azure-devops
+    # Step 1 is REGISTERING THE APP. This is done manually on the referenced portal
+    # Step 2 of the MS docs: AUTHORIZING THE APP. Here, the user is authorizing the app to do certain things.
 def oauth(request):
     code = request.GET.get('code', '')
     state = request.GET.get('state', '')
 
+    # Save the authorization token
+    file = open("oneOauthTaskApp/auth codes/authorization_code.txt", "w")
+    file.write(code)
     return render(request, 'oneOauthTaskApp/oauth.html')
 
+    # Step 3 of the MS docs: GET ACCESS AND REFRESH TOKENS FOR THE USER. This establishes the app and server with the 3p auth server.
 def oauth_token(request):
-    # api-endpoint
+
     url = """https://app.vssps.visualstudio.com/oauth2/token"""
 
-
-    # defining a params dict for the parameters to be sent to the API
-    headers = { 
-                "Content-Type": "application/x-www-form-urlencoded",
-                "Content-Length": "1322"
-            }
+    # Retrieve the authorization token
+    file = open("oneOauthTaskApp/auth codes/authorization_code.txt", "r")
+    code = file.read()
     
+    #from the authorization portal used in step 1
     app_secret = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIs"
-
     callback_url = "https://ado-oauth.azurewebsites.net"
 
     body = {
             f"client_assertion_type=urn:ietf:params:oauth:client-assertion-type:jwt-bearer&client_assertion={app_secret}&grant_type=urn:ietf:params:oauth:grant-type:jwt-bearer&assertion={code}&redirect_uri={callback_url}"
     }
-    print(body)
-    # sending post request and saving response as response object
-    r = requests.post(url = url, headers=headers, json=body)
+
+    r = requests.post(url = url, json=body)
     
-    # extracting data in json format
     data = r.json()
     
     return render(request, 'oneOauthTaskApp/oauth_token.html', {"data": data})
